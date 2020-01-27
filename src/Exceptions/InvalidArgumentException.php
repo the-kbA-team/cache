eb4e8af550d59e3ec38332ce7e1bc19f7dfbdfc9
gregor-j <gregor-j@users.noreply.github.com>
2020-01-27 09:35:53 +0100

    remove senseless proxy function

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -2,8 +2,6 @@
 
 namespace kbATeam\Cache\Exceptions;
 
-use Throwable;
-
 /**
  * Class kbATeam\Cache\Exceptions\InvalidArgumentException
  *
@@ -18,20 +16,4 @@ use Throwable;
  */
 class InvalidArgumentException extends \Exception implements \Psr\SimpleCache\InvalidArgumentException
 {
-    /**
-     * Construct the exception. Note: The message is NOT binary safe.
-     * @link  http://php.net/manual/en/exception.construct.php
-     * @param string    $message  [optional] The Exception message to throw.
-     * @param int       $code     [optional] The Exception code.
-     * @param \Throwable $previous [optional] The previous throwable used for the
-     *                            exception chaining.
-     * @since 5.1.0
-     */
-    public function __construct(
-        $message = '',
-        $code = 0,
-        Throwable $previous = null
-    ) {
-        parent::__construct($message, $code, $previous);
-    }
 }
